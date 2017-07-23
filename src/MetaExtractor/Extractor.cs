08a07a951be6c312dbd56b1355bf4c0714765ccf
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2017-07-23 20:57:51 +0300

    Update metadata extracter implementations

--- src/MetaExtractor/Extractor.cs
+++ src/MetaExtractor/Extractor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,10 +25,40 @@ namespace MetaExtractor
             var image = ReadOpenGraphProperty(document, "og:image");
             var description = ReadOpenGraphProperty(document, "og:description");
 
+            if (String.IsNullOrEmpty(title))
+            {
+                var node = document.DocumentNode.SelectSingleNode("//head/title");
+                title = node != null ? node.InnerText : "";
+            }
+
+            if (String.IsNullOrEmpty(image))
+            {
+                var images = document.DocumentNode.Descendants("img")
+                    .Select(e => e.GetAttributeValue("src", null))
+                    .Where(s => !String.IsNullOrEmpty(s))
+                    .ToList();
+
+                image = images.FirstOrDefault();
+            }
 
             if (String.IsNullOrEmpty(image) && !String.IsNullOrEmpty(DefaultImage))
             {
                 image = DefaultImage;
+            }
+
+            if (String.IsNullOrEmpty(description))
+            {
+                var node = document.DocumentNode.SelectSingleNode("//meta[@name='description']");
+                description = node != null ? node.Attributes["content"].Value : "";
+            }
+
+            if (String.IsNullOrEmpty(description))
+            {
+                var node = document.DocumentNode.SelectSingleNode("//body");
+                var maxLength = node.InnerText.Length;
+                var length = maxLength >= 300 ? 300 : maxLength;
+
+                description = node.InnerText.Substring(0, length);
             }
 
             return new Metadata
@@ -45,7 +76,7 @@ namespace MetaExtractor
             var node = document.DocumentNode.SelectSingleNode($"//meta[@property='{name}']");
 
 
-            if (node != null && node.Attributes["content"] != null)
+            if (node?.Attributes["content"] != null)
             {
                 return node.Attributes["content"].Value;
             }
