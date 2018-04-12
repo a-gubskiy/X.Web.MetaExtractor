71af5a72ab63e0ba360e109cfba079a24a1c86dc
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2018-04-12 13:05:30 +0300

    Created example app

---
+++ example/X.Web.MetaExtractor.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace X.Web.MetaExtractor.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
