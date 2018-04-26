affa46f9ad808796d995108fa872cf0b269482f2
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2018-04-26 15:46:57 +0300

    Create IExtractor interface

---
+++ src/X.Web.MetaExtractor/IExtractor.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Threading.Tasks;
+
+namespace X.Web.MetaExtractor
+{
+    public interface IExtractor
+    {
+        Task<Metadata> ExtractAsync(Uri uri);
+        Metadata Extract(Uri uri);
+    }
+}
