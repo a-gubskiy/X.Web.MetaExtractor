da0272282dc541821b377aeff76193c3fdbdc0fc
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2018-04-26 15:47:11 +0300

    Create IPageContentLoader interface

---
+++ src/X.Web.MetaExtractor/IPageContentLoader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace X.Web.MetaExtractor
+{
+    public interface IPageContentLoader
+    {
+        /// <summary>
+        /// Load html
+        /// </summary>
+        /// <param name="uri">Page url</param>
+        /// <returns></returns>
+        string LoadPageContent(Uri uri);
+        
+        /// <summary>
+        /// Load html
+        /// </summary>
+        /// <param name="uri">Page url</param>
+        /// <returns></returns>
+        Task<string> LoadPageContentAsync(Uri uri);
+    }
+}
