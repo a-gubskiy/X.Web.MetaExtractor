be669c6f5f3d6ba2b9a5835d81d6e26e04a32c00
Andrew Gubskiy <andrew.gubskiy@outlook.com>
2024-07-09 21:47:48 +0300

    Update

--- src/X.Web.MetaExtractor/ContentLoaders/HttpClient/HttpClientFactory.cs
+++ src/X.Web.MetaExtractor/ContentLoaders/HttpClient/HttpClientFactory.cs
@@ -14,16 +14,16 @@ public class HttpClientFactory : IHttpClientFactory
 
     private static System.Net.Http.HttpClient Create(string key)
     {
-        var handler = new HttpClientHandler {AllowAutoRedirect = true};
+        var handler = new HttpClientHandler { AllowAutoRedirect = true };
 
-        var client = new System.Net.Http.HttpClient(handler) {Timeout = TimeSpan.FromSeconds(5)};
+        var client = new System.Net.Http.HttpClient(handler) { Timeout = TimeSpan.FromSeconds(5) };
 
         client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "text/html,application/xhtml+xml,application/xml");
         client.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate");
         client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "curl/7.54.0 (X.Web.MetaExtractor)");
         client.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Charset", "UTF-8");
         client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "text/html; charset=UTF-8");
-        
+
         return client;
     }
 }
