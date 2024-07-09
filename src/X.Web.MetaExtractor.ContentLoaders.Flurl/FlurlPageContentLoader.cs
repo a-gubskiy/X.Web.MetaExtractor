355b893bf2c31d1c037e72f66357292bf5100f5d
Andrew Gubskiy <andrew.gubskiy@outlook.com>
2024-07-09 13:34:10 +0300

    Add Flurl implementation

---
+++ src/X.Web.MetaExtractor.ContentLoaders.Flurl/FlurlPageContentLoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Flurl.Http;
+using JetBrains.Annotations;
+
+namespace X.Web.MetaExtractor.ContentLoaders.Refit;
+
+[PublicAPI]
+public class FlurlPageContentLoader : IPageContentLoader
+{
+    public async Task<string> LoadPageContentAsync(Uri uri)
+    {
+        var html = await uri.ToString().GetStringAsync();
+        
+        return html;
+    }
+}
