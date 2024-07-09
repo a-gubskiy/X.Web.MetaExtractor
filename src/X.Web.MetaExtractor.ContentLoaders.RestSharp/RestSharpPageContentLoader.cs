aa74133c3a7bac6391422d203c0bc1ec80373933
Andrew Gubskiy <andrew.gubskiy@outlook.com>
2024-07-09 11:52:16 +0300

    Fix code style

--- src/X.Web.MetaExtractor.ContentLoaders.RestSharp/RestSharpPageContentLoader.cs
+++ src/X.Web.MetaExtractor.ContentLoaders.RestSharp/RestSharpPageContentLoader.cs
@@ -4,29 +4,28 @@ using System.Threading.Tasks;
 using JetBrains.Annotations;
 using RestSharp;
 
-namespace X.Web.MetaExtractor.ContentLoaders.RestSharp
+namespace X.Web.MetaExtractor.ContentLoaders.RestSharp;
+
+[PublicAPI]
+public class RestSharpPageContentLoader : IPageContentLoader
 {
-    [PublicAPI]
-    public class RestSharpPageContentLoader : IPageContentLoader
+    public async Task<string> LoadPageContentAsync(Uri uri)
     {
-        public async Task<string> LoadPageContentAsync(Uri uri)
+        if (uri == null)
         {
-            if (uri == null)
-            {
-                throw new ArgumentNullException(nameof(uri));
-            }
-
-            var client = new RestClient();
-            var request = new RestRequest(uri, Method.Get);
-
-            var response = await client.ExecuteAsync(request);
-
-            if (!response.IsSuccessful)
-            {
-                throw new HttpRequestException($"Error fetching content from {uri}. Status code: {response.StatusCode}");
-            }
-
-            return response.Content;
+            throw new ArgumentNullException(nameof(uri));
         }
+
+        var client = new RestClient();
+        var request = new RestRequest(uri, Method.Get);
+
+        var response = await client.ExecuteAsync(request);
+
+        if (!response.IsSuccessful)
+        {
+            throw new HttpRequestException($"Error fetching content from {uri}. Status code: {response.StatusCode}");
+        }
+
+        return response.Content;
     }
 }
